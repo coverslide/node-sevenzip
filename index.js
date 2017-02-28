96db306be160e4f30071ef122e5d9f462ff16602
Richard Hoffman <coverslide@gmail.com>
2017-02-28 00:21:13 -0800

    Update coding standards, customize executable

--- index.js
+++ index.js
@@ -1,79 +1,73 @@
-'use strict';
 
-var Path = require('path');
-var spawnAsync = require('child_promise');
 
-var tmp = require('tmp');
+const Path = require('path');
+const spawnAsync = require('child_promise');
 
-var SevenZip = module.exports = {};
+const tmp = require('tmp');
 
-SevenZip.getFilesAsync = function (fullPath) {
-    return runChildProcessAsync(['l', fullPath])
-        .then(function (output) {
-            return new Promise(function (resolve, reject) {
-                resolve(getFilesFromOutput(output));
-            });
-        })
-};
+const SevenZip = { executable: '7z' };
 
-SevenZip.getSingleFileAsync = function (fullPath, filename) {
-    return new Promise(function (resolve, reject) {
-            tmp.dir(function (err, path) {
-                if (err) return reject(err);
-                resolve(path);
-            });
-        }).then(function (dir) {
-            var args = ['x', fullPath, '-o' + dir, filename];
-            return runChildProcessAsync(args)
-                .then(function (output) {
-                    return new Promise(function (resolve, reject) {
-                        resolve(Path.join(dir, filename));
-                    });
-                });
-        });
-};
+module.exports = SevenZip;
 
 function runChildProcessAsync(args, options) {
-    return spawnAsync('7z', args, options)
-        .then(function (output) {
-            return output.split(/[\n\r]+/).slice(3).join('\n');
-        });
+  return spawnAsync(SevenZip.executable, args, options)
+    .then(output => output.split(/[\n\r]+/).slice(3).join('\n'));
 }
 
-function getFilesFromOutput (output) {
-    var lines = output.split(/[\n\r]+/);
-    var fileLines = false;
-    var lineRegExp = /^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.*)\s*$/;
-    var files = [];
-    lines.forEach(function (line) {
-        if (line.match(/----------/)) {
-            fileLines = !fileLines;
-            var lineLengthMatches = line.match(/(-+)(\s+)(-+)(\s+)(-+)(\s+)(\-+)(\s+)/);
-            var lineLengths = Array.prototype.map.call(lineLengthMatches, function (match) {
-                return match.length
-            });
-            lineRegExp = new RegExp(
-                '^\\s*'
-                + '(.{' + lineLengths[1] +'})\\s{' + lineLengths[2] + '}'
-                + '(.{' + lineLengths[3] +'})\\s{' + lineLengths[4] + '}'
-                + '(.{' + lineLengths[5] +'})\\s{' + lineLengths[6] + '}'
-                + '(.{' + lineLengths[7] +'})\\s{' + lineLengths[8] + '}'
-                + '(.*)\\s*$');
-            return;
-        }
-        if (!fileLines) {
-            //check if we are above the fileLines
-            return;
-        }
-        var lineAttributesMatch = line.match(lineRegExp);
-        var lineAttributes = {
-            datetime: lineAttributesMatch[1],
-            attr: lineAttributesMatch[2],
-            usize: +lineAttributesMatch[3],
-            csize: +lineAttributesMatch[4],
-            filename: lineAttributesMatch[5]
-        };
-        files.push(lineAttributes);
-    });
-    return files;
+function getFilesFromOutput(output) {
+  const lines = output.split(/[\n\r]+/);
+  let fileLines = false;
+  let lineRegExp = /^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.*)\s*$/;
+  const files = [];
+  lines.forEach((line) => {
+    if (line.match(/----------/)) {
+      fileLines = !fileLines;
+      const lineLengthMatches = line.match(/(-+)(\s+)(-+)(\s+)(-+)(\s+)(-+)(\s+)/);
+      const lineLengths = Array.prototype.map.call(lineLengthMatches, match => match.length);
+      lineRegExp = new RegExp(
+        `${'^\\s*'
+        + '(.{'}${lineLengths[1]}})\\s{${lineLengths[2]}}`
+        + `(.{${lineLengths[3]}})\\s{${lineLengths[4]}}`
+        + `(.{${lineLengths[5]}})\\s{${lineLengths[6]}}`
+        + `(.{${lineLengths[7]}})\\s{${lineLengths[8]}}`
+        + '(.*)\\s*$');
+      return;
+    }
+    if (!fileLines) {
+      // check if we are above the fileLines
+      return;
+    }
+    const lineAttributesMatch = line.match(lineRegExp);
+    const lineAttributes = {
+      datetime: lineAttributesMatch[1],
+      attr: lineAttributesMatch[2],
+      usize: +lineAttributesMatch[3],
+      csize: +lineAttributesMatch[4],
+      filename: lineAttributesMatch[5],
+    };
+    files.push(lineAttributes);
+  });
+  return files;
 }
+
+SevenZip.getFilesAsync = function getFilesAsync(fullPath) {
+  return runChildProcessAsync(['l', fullPath])
+    .then(output => new Promise((resolve) => {
+      resolve(getFilesFromOutput(output));
+    }));
+};
+
+SevenZip.getSingleFileAsync = function getSingleFileAsync(fullPath, filename) {
+  return new Promise((resolve, reject) => {
+    tmp.dir((err, path) => {
+      if (err) return reject(err);
+      return resolve(path);
+    });
+  }).then((dir) => {
+    const args = ['x', fullPath, `-o${dir}`, filename];
+    return runChildProcessAsync(args)
+      .then(() => new Promise((resolve) => {
+        resolve(Path.join(dir, filename));
+      }));
+  });
+};
